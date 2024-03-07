e7c1ccfab4db5460214de96f044e84ba1894fd98
Danielle <77177477+DanielleCreary-Thomas@users.noreply.github.com>
2024-03-07 13:22:45 -0500

    Adding Github to main

--- src/Time.java
+++ src/Time.java
@@ -19,6 +19,7 @@ import javax.swing.JOptionPane;
  */
 public class Time
 {
+	/*
 	public static void main(String[] args) 
 	{
 		try
@@ -48,6 +49,22 @@ public class Time
 		catch(Exception e)
 		{
 			System.out.println("An unexpected Exception occurred");
+		}
+	}
+	*/
+	public static void main(String[] args) {
+
+		double totalSeconds = Time.getTotalSeconds("10:10:10:99");
+
+		System.out.println("**GH total Seconds = "+ totalSeconds);
+		//TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+		// to see how IntelliJ IDEA suggests fixing it.
+		System.out.printf("Hello and welcome!");
+
+		for (int i = 1; i <= 5; i++) {
+			//TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+			// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+			System.out.println("i = " + i);
 		}
 	}
 	//hh:mm:ss:ms
